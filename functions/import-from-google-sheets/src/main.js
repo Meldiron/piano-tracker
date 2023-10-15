580dc4061239ba706144324e4ac6a3d475a2cc71
Matej Bačo <matejbaco2000@gmail.com>
2023-10-15 14:44:23 +0200

    Implement CSV parser

--- functions/import-from-google-sheets/src/main.js
+++ functions/import-from-google-sheets/src/main.js
@@ -1,33 +1,20 @@
-import { Client } from 'node-appwrite';
+import { Client, Storage } from 'node-appwrite';
+import * as csv from 'csv-string';
 
-// This is your Appwrite function
-// It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+  const client = new Client()
+     .setEndpoint('https://cloud.appwrite.io/v1')
+     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+     .setKey(process.env.APPWRITE_API_KEY);
 
-  // You can log messages to the console
-  log('Hello, Logs!');
+    const fileId = req.body;
 
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
+    const storage = new Storage(client);
+    const file = await storage.getFileDownload('imports', fileId);
 
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
-  }
+    const rows = csv.parse(file);
 
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build Fast. Scale Big. All in One Place.',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
-  });
+    return res.send({
+      output: rows
+    });
 };
